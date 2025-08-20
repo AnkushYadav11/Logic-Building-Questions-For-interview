303029da49d6f04a3d05906464294092f6d31fd4
AnkushYadav <yadavankush1811@gmail.com>
2025-08-20 14:47:16 +0530

    .

---
+++ Acending.java
@@ -0,0 +1,17 @@
+class Acending{
+	public static void main(String[] args){
+		int[] arr = {2,5,1,4,3};
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=0;j<arr.length-1-i;j++){
+				if(arr[j] > arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		for(int n : arr){
+			System.out.println(n + " ");
+		}
+	}
+}
