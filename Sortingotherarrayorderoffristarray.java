3a5c204e65c99b093629ded2a6314c59cacaf544
Ankush Yadav <yadavankush1811@gmail.com>
2025-09-04 11:42:27 +0530

    Sorting an array according to another array

---
+++ Sortingotherarrayorderoffristarray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Sortingotherarrayorderoffristarray {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {5,4,3,2,1};
+        for(int i=0; i<arr1.length;i++){
+            if(arr1[i] != arr2[i]){
+                for(int x=0;x<arr2.length;x++){
+                    for(int y=0;y<arr2.length;y++){
+                        if(arr1[i] == arr2[y]){
+                            int temp = arr2[x];
+                            arr2[x] = arr2[y];
+                            arr2[y] = temp;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("First Array is:- "+Arrays.toString(arr1));
+        System.out.println("Second Sorted Array is:- "+Arrays.toString(arr2));
+    }
+}
