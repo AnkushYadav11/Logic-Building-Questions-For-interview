4b74a5433011531b9ca837ca89c12a6e590afa8d
Ankush Yadav <yadavankush1811@gmail.com>
2025-09-05 13:17:20 +0530

    Repeting char in string.

--- Repetingchar.java
+++ Repetingchar.java
@@ -1,15 +1,15 @@
-public class Repetingstring {
+public class Repetingchar {
     public static void main(String[] args) {
         String str = "Hello world";
-        int count = 0;
+        int count=0;
         for(int i=0;i<str.length();i++){
             for(int j=i+1;j<str.length();j++){
                 if(str.charAt(i) == str.charAt(j)){
                     count++;
                 }
             }
-            System.out.println(str.charAt(i)+" Found "+count+" Times.");
-            count=0;
+            System.out.println(str.charAt(i)+" is repiting "+ count + " Times.");
+            count = 0;
         }
     }
 }
