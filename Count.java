366273ddea37055c4b924cd3a76d6968e9f093de
Ankush Yadav <yadavankush1811@gmail.com>
2025-09-15 12:53:14 +0530

    Counting of word which is starts with A.

--- Count.java
+++ Count.java
@@ -1,6 +1,14 @@
 public class Count {
-   public static void main(String[] args) {
-    String str = "Hello i Am Ankush";
-    for(int i= )
-   } 
+    public static void main(String[] args) {
+        String str = "Hello i Am Ankush";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                if (str.charAt(i + 1) == 'A') {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Number of words with char 'A' is:-"+count);
+    }
 }
